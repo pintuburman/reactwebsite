abbf5872f94d9f38878b426db4be0ebaeb6e1f9e
unknown <pintuburman08@gmail.com>
2022-01-20 21:46:15 +0530

    Updates

---
+++ precache-manifest.479516bfbf1ff330c8de7471e65b50a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445e070a5461efa2f8160a48225b81d3",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "4230a16f525e6f6f4450",
+    "url": "/reactwebsite/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "543cabe176b427618086",
+    "url": "/reactwebsite/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4230a16f525e6f6f4450",
+    "url": "/reactwebsite/static/js/2.1f750dfe.chunk.js"
+  },
+  {
+    "revision": "655fe1944359f55e29ac",
+    "url": "/reactwebsite/static/js/3.7cea0718.chunk.js"
+  },
+  {
+    "revision": "543cabe176b427618086",
+    "url": "/reactwebsite/static/js/main.da5b2fa4.chunk.js"
+  },
+  {
+    "revision": "4293ea1034eb0da90dc0",
+    "url": "/reactwebsite/static/js/runtime~main.4cc4388c.js"
+  }
+]);
